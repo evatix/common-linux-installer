5819e811417a1ced5317beaaedfaa5f9f18978ef
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-08-28 16:47:23 +0600

    Updates

--- vmware/vmware-mount-shared.sh
+++ vmware/vmware-mount-shared.sh
@@ -6,4 +6,4 @@ cd ~/Desktop/SharedDirectories
 # add to startup
 # https://serverfault.com/questions/735621/debian-how-to-run-a-script-on-startup-without-having-to-write-30-lines-of-shell
 crontab -l
-crontab -e ./vmware-mount-shared.sh
+cp -rf ./ ~/Desktop/vmware
