478e5e04c401e61da25d79ff004e22d83748a128
aukgit <devorg.bd@gmail.com>
2020-08-31 17:21:59 +0600

    Updated

--- chrome/chrome.sh
+++ chrome/chrome.sh
@@ -1,13 +1,16 @@
 sudo apt update 
 FILE=./google-chrome-stable_current_amd64.deb 
-if ![test -f "$FILE"]; then 
-    echo "$FILE doesn't exist so downloading." 
+ParentDirectory= /home/$USER/Downloads/chrome
+mkdir ParentDirectory
+Path = "$ParentDirectory/$FILE"
+if test -f "$Path"; then 
+    echo "$Path already present." 
+    ls | grep "chrome" 
+else 
+    echo "$Path doesn't exist so downloading." 
     # https://stackoverflow.com/questions/1078524/how-to-specify-the-location-with-wget
     wget -p "/home/$USER/Downloads/chrome" https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-else 
-    echo "file exist" 
-    ls | grep "chrome" 
 fi 
 
-yes | sudo apt --fix-broken install ./google-chrome-stable_current_amd64.deb 
+yes | sudo apt --fix-broken install $Path
 sudo apt update 
