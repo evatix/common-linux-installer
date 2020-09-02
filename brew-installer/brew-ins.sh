986a7218e9b33740a752edcc5b97200b2037c8de
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-09-02 23:54:31 +0600

    Updates

--- brew-installer/brew-ins.sh
+++ brew-installer/brew-ins.sh
@@ -1,5 +1,7 @@
 echo "make ins-devenv, if already not."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 bash ./brew-eval.sh
+# https://docs.brew.sh/Homebrew-on-Linux
+echo "Visit: https://docs.brew.sh/Homebrew-on-Linux"
 sudo apt update
 sudo reboot
