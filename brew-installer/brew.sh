14d2eec7f7f5d69082aec4b73ece760d4ca81b7f
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-09-02 23:45:10 +0600

    Updates

--- brew-installer/brew.sh
+++ brew-installer/brew.sh
@@ -1,7 +1,5 @@
-cd ..
 echo "make ins-devenv, if already not."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-cd "/brew-installer"
-brew
+bash ./brew-eval.sh
 sudo apt update
 sudo reboot
