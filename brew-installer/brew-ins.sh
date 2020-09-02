cd14e77c71e4797c1bbb71fae18bb5970f697fd0
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-09-03 01:26:35 +0600

    updates

--- brew-installer/brew-ins.sh
+++ brew-installer/brew-ins.sh
@@ -3,7 +3,6 @@ echo "make ins-devenv, if already not."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 sh ./brew-eval.sh
 echo "https://github.com/Linuxbrew/brew"
-
 # https://docs.brew.sh/Homebrew-on-Linux
 echo "Visit: https://docs.brew.sh/Homebrew-on-Linux"
 echo "Packages: https://formulae.brew.sh/formula-linux/"
