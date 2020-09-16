8cd4936e53422b7e15db8f729469135331700daf
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-09-16 12:13:46 +0600

    Updates

--- shared/service-helper.sh
+++ shared/service-helper.sh
@@ -7,7 +7,9 @@ create_and_run_service() {
     local systemPath="/etc/systemd/system/$serviceFileName"
 
     # cp akarim-service.service /etc/systemd/system/akarim-service.service
+    echo "Copying cp $serviceFilePath $systemPath ..."
     sudo cp $serviceFilePath $systemPath
+    echo "chmod 644 $systemPath ..."
     sudo chmod 644 $systemPath
     service_start $serviceName
     service_status $serviceName
@@ -19,29 +21,36 @@ create_and_run_service() {
 service_start() {
     local serviceName=$1
     local serviceFileName=$serviceName.service
+    echo "$serviceFileName - service starting..."
     sudo systemctl start $serviceFileName
 }
 
 service_status() {
     local serviceName=$1
     local serviceFileName=$serviceName.service
+    echo "$serviceFileName - service status..."
     sudo systemctl start $serviceFileName
 }
 
 service_enable() {
     local serviceName=$1
     local serviceFileName=$serviceName.service
+    echo "$serviceFileName - service enabled..."
     sudo systemctl enable $serviceFileName
 }
 
 service_restart() {
     local serviceName=$1
     local serviceFileName=$serviceName.service
+    echo "$serviceFileName - service restarting..."
     sudo systemctl restart $serviceFileName
 }
 
-service_restart() {
+service_full_restart() {
     local serviceName=$1
     local serviceFileName=$serviceName.service
+    echo "Service full restarting (stop, start, restart)..."
+    sudo systemctl stop $serviceFileName
+    sudo systemctl start $serviceFileName
     sudo systemctl restart $serviceFileName
 }
--- shared/test.sh
+++ shared/test.sh
@@ -4,4 +4,5 @@
 . ./service-helper.sh
 
 create_and_run_service brew-service ../brew-installer/brew-service.service
+service_status brew-service
 # sudo cp brew-service.service /etc/systemd/system/brew-service.service
