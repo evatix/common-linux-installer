1507f199a40645c808e07d8b07f8f5c67e77dfa8
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-09-16 19:20:48 +0600

    Updates

--- shared/test.sh
+++ shared/test.sh
@@ -9,8 +9,8 @@ service_test(){
     create_and_run_service brew-service.server ../brew-installer/brew-service.server.service
     service_status brew-service.server
     service_status_all
-    service_remove brew-service.server
-    service_status_all
+    # service_remove brew-service.server
+    # service_status_all
 
     # sudo cp brew-service.service /etc/systemd/system/brew-service.service
 }
