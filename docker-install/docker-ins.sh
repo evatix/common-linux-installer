700f23b320caae202f31a04be578efe0165f5931
Md. Alim Ul Karim <alim.karim@evatix.com>
2020-12-24 18:04:52 +0600

    Update

--- docker-install/docker-ins.sh
+++ docker-install/docker-ins.sh
@@ -4,9 +4,11 @@ sudo apt-get update
 mkdir -p ~/scripts
 cd ~/scripts
 sudo curl -fsSL https://get.docker.com -o get-docker.sh | sh
+sudo apt-get update
 docker --version
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
 newgrp - docker
+
 docker run hello-world
