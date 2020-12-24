86f87f0e795b11b75e68b97d0495863c322382d0
Md. Alim Ul Karim <alim.karim@evatix.com>
2020-12-24 20:57:28 +0600

    Updates

--- docker-install/docker-ins.sh
+++ docker-install/docker-ins.sh
@@ -12,3 +12,11 @@ sudo chmod 666 /var/run/docker.sock
 newgrp - docker
 
 docker run hello-world
+
+## Docker machine install
+
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
+docker-machine version
