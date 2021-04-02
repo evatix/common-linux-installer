fcf51fc599f9e1e1eac76c47bfe0a72bbed061af
Md. Alim Ul Karim <aukgit@users.noreply.github.com>
2021-04-02 17:39:14 +0600

    Update git.sh
    sudo add-apt-repository ppa:git-core/ppa
    sudo apt-get install git

--- git-latest/git.sh
+++ git-latest/git.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 pwd
 echo $USER
-user=$USER
-echo "user: $USER"
-echo "uid: $UID"
-echo "whoami: $whoami"
+# user=$USER
+# echo "user: $USER"
+# echo "uid: $UID"
+# echo "whoami: $whoami"
 # cat /etc/passwd | cut -d":" -f1,3
 #https://bit.ly/3gIJCPE
-currentDir="/home/$user/Downloads/temp"
-echo $currentDir
+# currentDir="/home/$user/Downloads/temp"
+# echo $currentDir
 # mkdir $currentDir
-cd $currentDir
-ls -la
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get install git
+git version
 pwd
