3f65ddc5728d683006013cefb55825f95d7270c2
Md. Alim Ul Karim <aukgit@users.noreply.github.com>
2021-04-02 17:50:55 +0600

    Update golang.ins.sh
    # reference : https://golang.org/doc/install
    
    mkdir downloads
    cd downloads
    wget https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
    sudo rm -rf /usr/local/go 
    sudo tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz
    export PATH=$PATH:/usr/local/go/bin
    go version
    cd ..
    rm -rf downloads

--- golang-ins/golang.ins.sh
+++ golang-ins/golang.ins.sh
@@ -5,7 +5,9 @@
 mkdir downloads
 cd downloads
 wget https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go 
+sudo tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+go version
 cd ..
 rm -rf downloads
