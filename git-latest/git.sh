3e3255737b2f141aec14870184143fa7cef1001e
Md. Alim Ul Karim <aukgit@users.noreply.github.com>
2021-04-09 15:33:45 +0600

    Update git.sh

--- git-latest/git.sh
+++ git-latest/git.sh
@@ -13,4 +13,20 @@ echo $USER
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get install git
 git version
+
+## centos https://gist.github.com/egorsmkv/30faa3e61c185a41e89cf849737d4d4b
+sudo su
+yum groupinstall 'Development Tools'
+yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-CPAN perl-devel
+
+mkdir -p downloads
+cd downloads
+wget https://github.com/git/git/archive/refs/tags/v2.31.1.tar.gz
+tar xf v2.31.1.tar.gz
+cd v2.31.1
+make ./Configure
+make all
+make install
+cd ..
+
 pwd
